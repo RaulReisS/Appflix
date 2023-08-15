2402a1ff95cbe5212bbe140a547906a1d8dbc087
Raul Reis <raul.soares@usp.br>
2023-08-15 16:06:36 -0300

    Criação da estrutura de programação para utilizar a Recycler View, colocando a classe adapter em uma classe externa a Activity

--- app/src/main/java/br/com/raulreis/appflix/MainActivity.kt
+++ app/src/main/java/br/com/raulreis/appflix/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.raulreis.appflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainAdapter()
+        val rv: RecyclerView = findViewById(R.id.rvMain)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
     }
 }
